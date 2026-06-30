a5ccd523c8243392a5b1e5f41d08df9787589223
sebasGZA <sebassga34@gmail.com>
2026-06-30 15:22:53 -0500

    feat: pages structure

--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+import { MenuItem } from '../../interfaces/menu-item.intercace';
+import { reactiveRoutes } from './../../../reactive/reactive.route';
+
+const reactiveRoute = reactiveRoutes[0].children ?? [];
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+  reactiveMenu: MenuItem[] = reactiveRoute
+    .filter((item) => item.path !== '**')
+    .map(item => ({
+      route: `reactive/${item.path}`,
+      title: `${item.title}`,
+    }));
+
+  authMenu: MenuItem[] = [
+    {
+      title: 'Register',
+      route: 'auth/sign-up',
+    },
+  ];
+
+  countryMenu: MenuItem[] = [
+    {
+      title: 'Countries',
+      route: './country'
+    }
+  ]
+}
