8a5a0fd7f4c766e8ec1f73ae23f7287fad17018f
sebasGZA <sebassga34@gmail.com>
2026-06-30 15:36:40 -0500

    feat: FormGroup and FormControl

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,15 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms'
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent {}
+export class BasicPageComponent {
+  myForm = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    stock: new FormControl(0),
+  })
+}
