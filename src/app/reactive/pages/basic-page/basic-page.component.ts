92dce42d03bdeb0184b1ca6d7be2a665c7545696
sebasGZA <sebassga34@gmail.com>
2026-06-30 15:42:33 -0500

    feat: FormBuilder

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,15 +1,21 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms'
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms'
 @Component({
   selector: 'app-basic-page',
   imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
 export class BasicPageComponent {
-  myForm = new FormGroup({
-    name: new FormControl(''),
-    price: new FormControl(0),
-    stock: new FormControl(0),
+  // myForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   stock: new FormControl(0),
+  // })
+  fb = inject(FormBuilder)
+  myForm = this.fb.group({
+    name: ['', [], []],
+    price: [0],
+    stock: [0],
   })
 }
